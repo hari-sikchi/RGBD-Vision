e6ea5cf50ceb7038d6937b9de9e63dd6c1af5c44
Arijit Kar <arijit.kar14@gmail.com>
2017-05-20 01:17:02 +0530

    Added lsd_slam:
    (1) Added the catkin and rosbuild workspaces containing all the required packages.
    (2) Added README with all the instructions regarding setup and use.

---
+++ ROS-Packages/lsd_slam/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src
+# Build directory: /home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(openni_launch)
+subdirs(openni_camera)
---
+++ ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_depth_image.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_depth_image.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_device.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_device.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_device_kinect.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_device_kinect.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_device_oni.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_device_oni.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_device_primesense.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_device_primesense.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_device_xtion.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_device_xtion.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_driver.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_driver.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_exception.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_exception.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_image_bayer_grbg.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_image_bayer_grbg.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_image_rgb24.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_image_rgb24.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_image_yuv_422.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_image_yuv_422.cpp.o"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/src/openni_ir_image.cpp" "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/build/openni_camera/CMakeFiles/openni_driver.dir/src/openni_ir_image.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"openni_camera\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/devel/include"
+  "/home/arijit/AK_Progs/RGBD-Vision/ROS-Packages/lsd_slam/catkin_ws/src/openni_camera/include"
+  "/usr/include/ni"
+  "/opt/ros/indigo/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
